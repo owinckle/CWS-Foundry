6c44c3574fcce004472f981c91f416bfc0e497fe
Ocean Winckler <oceanwinckler.rem@gmail.com>
2025-06-22 18:31:18 +0000

    Better logic

--- cws_foundry/main.py
+++ cws_foundry/main.py
@@ -1,9 +1,26 @@
 from services.google_sheet import GSM
+from time import sleep
 
 def main():
     # Initialize the GSM (Google Sheets Manager)
     gsm = GSM()
-    print(gsm.url)
+    gsm.fetch()
+
+    while True:
+        # Find the next available task
+        # Prioritize "Ready to publish"
+        task = gsm.get_next_task()
+
+        if not task:
+            print("No tasks found...")
+            continue
+
+        print("Executing task...")
+        sleep(5)
+        gsm.update_task({
+            "ID": task["ID"],
+            "Status": "Published"
+        })
 
 
 if __name__ == "__main__":
--- cws_foundry/services/google_sheet.py
+++ cws_foundry/services/google_sheet.py
@@ -42,6 +42,58 @@ from config.settings import Settings
 #     print("Request failed:", e)
 
 
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbw29moWDjYUYy3r3d3LaJHEJnuJd5Rn2Oe_4T0dyM7OVfav6Mrp2r_Ggfg-Gg0ksfiU/exec"
+SPREADSHEET_URL = f"https://docs.google.com/spreadsheets/d/{Settings.GS_SPREADSHEET_ID}/export?format=csv&gid=0"
+
+
 class GSM:
     def __init__(self):
-        self.url = f"https://docs.google.com/spreadsheets/d/{Settings.GS_SPREADSHEET_ID}/export?format=csv&gid=0"
+        self.tasks = []
+
+    """Fetch the data from the sheet"""
+    def fetch(self):
+        # Load sheet data starting from row 2 (header=1)
+        df = pd.read_csv(SPREADSHEET_URL, header=1)
+        df = df.replace({np.nan: None})
+        
+        # Map the headers
+        headers = df.columns.tolist()
+
+        # Retrieve the rows
+        data_dicts = []
+        for row in df.values.tolist():
+            row_dict = dict(zip(headers, row))
+            if not row_dict.get("ID"): # Stop if no ID in this row
+                break
+            data_dicts.append(row_dict)
+
+        self.tasks = data_dicts
+
+    """Find the next task and return it"""
+    def get_next_task(self):
+        next_task = None
+
+        for task in self.tasks[::-1]:
+            status = task["Status"]
+            if status == "Ready to publish":
+                next_task = task
+            elif status == "Ready":
+                next_task = task
+        
+        return next_task
+
+    """Update a specific task, given an ID"""
+    def update_task(self, payload):
+        if not payload["ID"]:
+            return False # No ID wa passed, failed request
+
+        try:
+            response = requests.post(WEBHOOK_URL, json=payload)
+            print("POST status code:", response.status_code)
+            print("Response text:", response.text)
+            try:
+                print("Response JSON:", response.json())
+            except Exception:
+                print("Response is not JSON format")
+        except Exception as e:
+            print("Request failed:", e)
